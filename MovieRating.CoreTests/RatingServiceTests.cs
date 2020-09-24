47f71fce9f48579cf529af7e958c8cd4dc50136d
Rado <radoslav.backovsky@gmail.com>
2020-09-24 14:52:05 +0200

    Moq added
    test rename

--- MovieRating.CoreTests/RatingServiceTests.cs
+++ MovieRating.CoreTests/RatingServiceTests.cs
@@ -3,3 +3,63 @@ using MovieRating.Core;
 using System;
 using System.Collections.Generic;
 using System.Text;
+
+namespace MovieRating.Core.Tests {
+    [TestClass()]
+    public class RatingServiceTests {
+        [TestMethod()]
+        public void GetAverageRateFromReviewerTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAverageRateOfMovieTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetMostProductiveReviewersTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetMoviesWithHighestNumberOfTopRatesTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetNumberOfRatesTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetNumberOfRatesByReviewerTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetNumberOfReviewsTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetNumberOfReviewsFromReviewerTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetReviewersByMovieTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTopMoviesByReviewerTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTopRatedMoviesTest() {
+            Assert.Fail();
+        }
+    }
+}
