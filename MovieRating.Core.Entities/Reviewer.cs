ea45ecf23ae31888e6d549640944558eaf3095f5
Rado <radoslav.backovsky@gmail.com>
2020-09-28 12:43:20 +0200

    BE

--- MovieRating.Core.Entities/Reviewer.cs
+++ MovieRating.Core.Entities/Reviewer.cs
@@ -1,7 +1,13 @@
-﻿namespace MovieRating.Core.Entities
+﻿using System.Collections.Generic;
+
+namespace MovieRating.Core.Entities
 {
     public class Reviewer
     {
-        
+        public int Id { get; set; }
+
+        public List<Rating> Ratings { get; set; }
+
+
     }
 }
