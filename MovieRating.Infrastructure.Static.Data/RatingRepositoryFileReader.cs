1017e23598a1802cecc02d44934cb22950d363dc
Martin Brodersen <martinroager@gmail.com>
2020-10-06 15:05:23 +0200

    Made the JSON FileReader class

---
+++ MovieRating.Infrastructure.Static.Data/RatingRepositoryFileReader.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using MovieRating.Core.DomainServices;
+using MovieRating.Core.Entities;
+using newtonsoft 
+
+namespace ConsoleApp
+{
+    public class RatingRepositoryFileReader : IRatingRepository
+    {
+        private readonly string _path = "ratings.json";
+
+        public RatingRepositoryFileReader()
+        {
+            GetReviewsFromFile(_path);
+        }
+
+        private IEnumerable<Review> _ratingCollection;
+
+        public IEnumerable<Review> GetAll()
+        {
+            return _ratingCollection;
+        }
+
+        public void GetReviewsFromFile(string _path)
+        {
+            using (StreamReader streamReader = File.OpenText(_path))
+            using (JsonTextReader reader = new JsonTextReader(streamReader))
+            {
+                reader.CloseInput = true;
+                var serializer = new JsonSerializer();
+                var ratings = new List<BERating>();
+
+                while (reader.Read())
+                {
+                    if (reader.TokenType == JsonToken.StartObject)
+                    {
+                        BERating review = serializer.Deserialize<BERating>(reader);
+                        ratings.Add(review);
+                    }
+
+                }
+                _ratingCollection = ratings;
+            }
+        }
+    }
+}
+}
